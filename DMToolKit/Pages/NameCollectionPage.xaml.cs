4696e8cb35eb14225a127cca9aab3b98bc100494
Wilson.Brian <brian.wilson@igt.com>
2023-03-19 16:16:39 -0700

    List Manager Page and Name Collection page altered functionality. NameGeneratorOptionsPage tweaked the button styling to not be linked to a style.

--- DMToolKit/Pages/NameCollectionPage.xaml.cs
+++ DMToolKit/Pages/NameCollectionPage.xaml.cs
@@ -4,9 +4,19 @@ namespace DMToolKit.Pages;
 
 public partial class NameCollectionPage : ContentPage
 {
+	NameCollectionViewModel viewModel;
 	public NameCollectionPage(NameCollectionViewModel vm)
 	{
 		InitializeComponent();
 		BindingContext = vm;
-	}
+		viewModel = vm;
+    }
+
+    private void ExpandClicked(object sender, EventArgs e)
+    {
+		if (viewModel.OptionsOpen)
+			ExpandButton.Source = "expanddark.png";
+		else
+			ExpandButton.Source = "retractdark.png";
+    }
 }
